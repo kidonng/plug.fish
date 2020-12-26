4d50a8f5eec33caae6ed967384b7eac18bd9af50
Kid <kidonng@gmail.com>
2020-12-26 20:20:17 +0800

    Hoist sub commands

--- functions/plug.fish
+++ functions/plug.fish
@@ -3,129 +3,58 @@ function plug -a cmd
     test -z "$plug_path" && set -U plug_path $XDG_DATA_HOME/fish/plug
     test -e $plug_path || command mkdir -p $plug_path
     set plug kidonng/fish-plug
+    set plugins $argv[2..-1]
+    set installed (_plug_list)
 
     switch "$cmd"
         case install add
-            set plugins $argv[2..-1]
-            set installed (plug list)
-
             for plugin in $plugins
                 if builtin contains $plugin $installed
                     echo $plugin is already installed
                     continue
                 end
 
-                echo Cloning $plugin
-                echo $plugin | read -d / owner repo
-                command git clone https://github.com/$plugin $plug_path/$owner/$repo
-
-                plug enable $plugin
+                _plug_install $plugin
             end
         case uninstall rm
-            set plugins $argv[2..-1]
-            set installed (plug list)
-
             for plugin in $plugins
                 if ! builtin contains $plugin $installed
                     echo $plugin is not installed
                     continue
                 end
 
-                plug disable $plugin
-
-                echo Removing $plugin
-                echo $plugin | read -d / owner repo
-                set owner_path $plug_path/$owner
-                command rm -rf $owner_path/$repo
-
-                set owner_plugins $owner_path/*
-                if test -z "$owner_plugins"
-                    command rm -r $owner_path
-                end
+                _plug_uninstall $plugin
             end
 
             if builtin contains $plug $plugins
                 command rm -r $plug_path
                 set -e plug_path
+
+                for func in install uninstall list enable disable update
+                    builtin functions -e _plug_$func
+                end
             end
         case list ls
-            set plugins (command find $plug_path -type d -mindepth 2 -maxdepth 2)
-
-            for plugin in $plugins
-                string join / (string split / $plugin)[-2..-1]
-            end
+            _plug_list
         case enable
-            set plugins $argv[2..-1]
-            set installed (plug list)
-
             for plugin in $plugins
                 if ! builtin contains $plugin $installed
                     echo $plugin is not installed
                     continue
                 end
 
-                echo Enabling $plugin
-                set plugin_path $plug_path/$plugin
-                set link_files
-
-                for file in $plugin_path/functions/*
-                    set -a link_files $file
-                    builtin source $file
-                end
-
-                set conf_path $plugin_path/conf.d
-                for file in $conf_path/*
-                    set -a link_files $file
-                    builtin source $file
-                    builtin emit (string replace $conf_path/ '' $file | string replace .fish _install)
-                end
-
-                for file in $plugin_path/completions/*
-                    set -a link_files $file
-                    builtin source $file
-                end
-
-                for file in $link_files
-                    command ln -s $file (string replace $plugin_path $__fish_config_dir $file)
-                end
+                _plug_enable $plugin
             end
         case disable
-            set plugins $argv[2..-1]
-            set installed (plug list)
-
             for plugin in $plugins
                 if ! builtin contains $plugin $installed
                     echo $plugin is not installed
                     continue
                 end
 
-                echo Disabling $plugin
-                set plugin_path $plug_path/$plugin
-                set unlink_files
-
-                set conf_path $plugin_path/conf.d
-                for file in $conf_path/*
-                    set -a unlink_files $file
-                    builtin emit (string replace $conf_path/ '' $file | string replace .fish _uninstall)
-                end
-
-                set func_path $plugin_path/functions
-                for file in $func_path/*
-                    set -a unlink_files $file
-                    builtin functions -e (string replace $func_path/ '' $file | string replace .fish '')
-                end
-
-                for file in $plugin_path/completions/*
-                    set -a unlink_files $file
-                end
-
-                for file in $unlink_files
-                    command rm (string replace $plugin_path $__fish_config_dir $file)
-                end
+                _plug_disable $plugin
             end
         case update up
-            set plugins $argv[2..-1]
-            set installed (plug list)
             test -z "$plugins" && set plugins $installed
 
             for plugin in $plugins
@@ -134,23 +63,106 @@ function plug -a cmd
                     continue
                 end
 
-                echo Updating $plugin
-                set plugin_path $plug_path/$plugin
-                command git -C $plugin_path fetch
-
-                set local (command git -C $plugin_path rev-parse HEAD)
-                set origin (command git -C $plugin_path rev-parse FETCH_HEAD)
-
-                if test $local != $origin
-                    plug disable $plugin
-                    command git -C $plugin_path pull --rebase
-
-                    if test $plugin = $plug
-                        builtin source $plugin_path/functions/plug.fish
-                    end
-
-                    plug enable $plugin
-                end
+                _plug_update $plugin
             end
+    end
+end
+
+function _plug_install -a plugin
+    echo Cloning $plugin
+    echo $plugin | read -d / owner repo
+    command git clone https://github.com/$plugin $plug_path/$owner/$repo
+
+    _plug_enable $plugin
+end
+
+function _plug_uninstall -a plugin
+    _plug_disable $plugin
+
+    echo Removing $plugin
+    echo $plugin | read -d / owner repo
+    set owner_path $plug_path/$owner
+    command rm -rf $owner_path/$repo
+
+    set owner_plugins $owner_path/*
+    if test -z "$owner_plugins"
+        command rm -r $owner_path
+    end
+end
+
+function _plug_list
+    set plugins (command find $plug_path -type d -mindepth 2 -maxdepth 2)
+
+    for plugin in $plugins
+        string join / (string split / $plugin)[-2..-1]
+    end
+end
+
+function _plug_enable -a plugin
+    echo Enabling $plugin
+    set plugin_path $plug_path/$plugin
+    set link_files
+
+    for file in $plugin_path/functions/*
+        set -a link_files $file
+        builtin source $file
+    end
+
+    set conf_path $plugin_path/conf.d
+    for file in $conf_path/*
+        set -a link_files $file
+        builtin source $file
+        builtin emit (string replace $conf_path/ '' $file | string replace .fish _install)
+    end
+
+    for file in $plugin_path/completions/*
+        set -a link_files $file
+        builtin source $file
+    end
+
+    for file in $link_files
+        command ln -s $file (string replace $plugin_path $__fish_config_dir $file)
+    end
+end
+
+function _plug_disable -a plugin
+    echo Disabling $plugin
+    set plugin_path $plug_path/$plugin
+    set unlink_files
+
+    set conf_path $plugin_path/conf.d
+    for file in $conf_path/*
+        set -a unlink_files $file
+        builtin emit (string replace $conf_path/ '' $file | string replace .fish _uninstall)
+    end
+
+    set func_path $plugin_path/functions
+    for file in $func_path/*
+        set -a unlink_files $file
+        builtin functions -e (string replace $func_path/ '' $file | string replace .fish '')
+    end
+
+    for file in $plugin_path/completions/*
+        set -a unlink_files $file
+    end
+
+    for file in $unlink_files
+        command rm (string replace $plugin_path $__fish_config_dir $file)
+    end
+end
+
+function _plug_update -a plugin
+    echo Updating $plugin
+    set plugin_path $plug_path/$plugin
+    command git -C $plugin_path fetch
+
+    set local (command git -C $plugin_path rev-parse HEAD)
+    set origin (command git -C $plugin_path rev-parse FETCH_HEAD)
+
+    if test $local != $origin
+        _plug_disable $plugin
+        command git -C $plugin_path pull --rebase
+
+        _plug_enable $plugin
     end
 end
