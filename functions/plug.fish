1d78e14dff2d17d593f0725d0be4e1cf46408cc7
Kid <kidonng@gmail.com>
2020-12-26 19:48:57 +0800

    Fix self updating

--- functions/plug.fish
+++ functions/plug.fish
@@ -2,6 +2,7 @@ function plug -a cmd
     test -z "$XDG_DATA_HOME" && set XDG_DATA_HOME ~/.local/share
     test -z "$plug_path" && set -U plug_path $XDG_DATA_HOME/fish/plug
     test -e $plug_path || command mkdir -p $plug_path
+    set plug kidonng/fish-plug
 
     switch "$cmd"
         case install add
@@ -138,6 +139,11 @@ function plug -a cmd
                 if test $local != $origin
                     plug disable $plugin
                     command git -C $plugin_path pull --rebase
+
+                    if test $plugin = $plug
+                        builtin source $plugin_path/functions/plug.fish
+                    end
+
                     plug enable $plugin
                 end
             end
