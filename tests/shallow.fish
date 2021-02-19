0626fbbd8290db6506c940d5661fd9c28b86f0f6
Hugo Haas <hugoh@hugoh.net>
2021-02-19 04:56:01 -0600

    Erroring out if there's a failure

--- tests/shallow.fish
+++ tests/shallow.fish
@@ -1,4 +1,10 @@
 source $GITHUB_WORKSPACE/functions/plug.fish
 
 plug install jorgebucaran/getopts.fish
-test (git -C $plug_path/jorgebucaran/getopts.fish rev-parse --is-shallow-repository) = true
+
+if test (git -C $plug_path/jorgebucaran/getopts.fish rev-parse --is-shallow-repository) = true
+    echo "Shallow git clone"
+else
+    echo "Full git clone"
+    exit 1
+end
