18b2c868ba8ed984875f949923618d9f25f2deae
Kid <kidonng@gmail.com>
2021-05-22 23:06:16 +0800

    Implement `list`, `init` `enable`/`disable`

--- completions/plug.fish
+++ completions/plug.fish
@@ -1,21 +1,19 @@
-complete plug -x -n __fish_use_subcommand -a "install uninstall list enable disable update pin unpin"
+complete plug -x -n __fish_use_subcommand -a "install uninstall list init enable disable update pin unpin"
 
-complete plug -x -n __fish_is_first_arg -s h -l help -d Help
+complete plug -x -n __fish_is_first_arg -s h -l help -d "Show usage information"
 
-complete plug -x -n "__fish_seen_subcommand_from uninstall rm" -a "(_plug_list)"
+complete plug -x -n "__fish_seen_subcommand_from uninstall rm" -a "(plug list)"
 
-complete plug -x -n "__fish_seen_subcommand_from list ls" # Disable file completion
-complete plug -x -n "__fish_seen_subcommand_from list ls" -s e -l enabled -d "List enabled plugins"
-complete plug -x -n "__fish_seen_subcommand_from list ls" -s d -l disabled -d "List disabled plugins"
-complete plug -x -n "__fish_seen_subcommand_from list ls" -s p -l pinned -d "List pinned plugins"
-complete plug -x -n "__fish_seen_subcommand_from list ls" -s u -l unpinned -d "List unpinned plugins"
-complete plug -x -n "__fish_seen_subcommand_from list ls" -s s -l source -d "Show plugin source"
-complete plug -x -n "__fish_seen_subcommand_from list ls" -s v -l verbose -d "Show plugin version and state"
+complete plug -x -n "__fish_seen_subcommand_from list ls"
+complete plug -n "__fish_seen_subcommand_from list ls" -s e -l enabled -d "List enabled plugins"
+complete plug -n "__fish_seen_subcommand_from list ls" -s d -l disabled -d "List disabled plugins"
+complete plug -n "__fish_seen_subcommand_from list ls" -s p -l pinned -d "List pinned plugins"
+complete plug -n "__fish_seen_subcommand_from list ls" -s u -l unpinned -d "List unpinned plugins"
 
-complete plug -x -n "__fish_seen_subcommand_from enable" -a "(_plug_list --disabled)"
+complete plug -x -n "__fish_seen_subcommand_from enable" -a "(plug list -d)"
 
-complete plug -x -n "__fish_seen_subcommand_from disable" -a "(_plug_list --enabled)"
+complete plug -x -n "__fish_seen_subcommand_from disable" -a "(plug list -e)"
 
-complete plug -x -n "__fish_seen_subcommand_from update up pin" -a "(_plug_list --unpinned)"
+complete plug -x -n "__fish_seen_subcommand_from update up pin" -a "(plug list -u)"
 
-complete plug -x -n "__fish_seen_subcommand_from unpin" -a "(_plug_list --pinned)"
+complete plug -x -n "__fish_seen_subcommand_from unpin" -a "(plug list -p)"
--- functions/plug.fish
+++ functions/plug.fish
@@ -1,4 +1,5 @@
 test -z "$plug_path" && set -U plug_path $__fish_user_data_dir/plug
+set _plug_regex '([^/]+)\.fish$'
 
 function plug -a cmd -d "Manage Fish plugins"
     set plugins (string match -v -- "-*" $argv[2..])
@@ -9,6 +10,7 @@ function plug -a cmd -d "Manage Fish plugins"
                 "Usage: plug "(set_color -o)"install"(set_color normal)"    <plugins>" \
                 "       plug "(set_color -o)"uninstall"(set_color normal)"  <plugins>" \
                 "       plug "(set_color -o)"list"(set_color normal)"       [options]" \
+                "       plug "(set_color -o)"init"(set_color normal) \
                 "       plug "(set_color -o)"enable"(set_color normal)"     <plugins>" \
                 "       plug "(set_color -o)"disable"(set_color normal)"    <plugins>" \
                 "       plug "(set_color -o)"update"(set_color normal)"     [plugins]" \
@@ -17,13 +19,118 @@ function plug -a cmd -d "Manage Fish plugins"
         case install add
         case uninstall rm
         case list ls
+            argparse e/enabled d/disabled -- $argv || return
+            set -q argv[2] && set filter $argv[2]
+
+            for author_path in $plug_path/*
+                set author (string replace $plug_path/ "" $author_path)
+
+                for plugin_path in $author_path/*
+                    set plugin (string replace $author_path/ "" $plugin_path)
+                    set plugin_full $author/$plugin
+
+                    if set -q _flag_enabled && ! contains $plugin_full $_plug_enabled
+                        or set -q _flag_disabled && contains $plugin_full $_plug_enabled
+                        or set -q filter && ! string match -rq $filter $plugin_full
+                        continue
+                    end
+
+                    if isatty stdout
+                        set plugin (set_color -o)$plugin(set_color normal)
+
+                        if ! contains $plugin_full $_plug_enabled
+                            set author (set_color -d)$author
+                        end
+                    end
+
+                    echo $author/$plugin
+                end
+            end
+        case init
+            if set -q _plug_initialized
+                echo plug: already initialized >&2
+                return 1
+            end
+
+            set -g _plug_initialized
+
+            for plugin_path in $plug_path/$_plug_enabled
+                test -d $plugin_path/completions && set -a complete_path $plugin_path/completions
+                test -d $plugin_path/functions && set -a function_path $plugin_path/functions
+            end
+
+            set fish_complete_path $fish_complete_path[1] $complete_path $fish_complete_path[2..]
+            set fish_function_path $fish_function_path[1] $function_path $fish_function_path[2..]
+
+            status -i || return 0
+
+            for file in $plug_path/$_plug_enabled/conf.d/*.fish
+                source $file
+            end
         case enable
+            for plugin in $plugins
+                set plugin_path $plug_path/$plugin
+
+                if ! test -d $plugin_path
+                    echo plug: (set_color -o)$plugin(set_color normal) does not exist >&2
+                    set _plug_error && continue
+                end
+
+                if contains $plugin $_plug_enabled
+                    echo plug: (set_color -o)$plugin(set_color normal) is already enabled >&2
+                    set _plug_error && continue
+                end
+
+                for file in $plugin_path/{completions,functions}/*.fish
+                    source $file
+                end
+
+                for file in $plugin_path/conf.d/*.fish
+                    source $file
+                    emit (string match -r $_plug_regex $file)[2]_install
+                end
+
+                set -Ua _plug_enabled $plugin
+                echo plug: (set_color -o)$plugin(set_color normal) enabled
+            end
         case disable
+            for plugin in $plugins
+                set plugin_path $plug_path/$plugin
+
+                if ! test -d $plugin_path
+                    echo plug: (set_color -o)$plugin(set_color normal) does not exist >&2
+                    set _plug_error && continue
+                end
+
+                if ! set index (contains -i $plugin $_plug_enabled)
+                    echo plug: (set_color -o)$plugin(set_color normal) is already disabled >&2
+                    set _plug_error && continue
+                end
+
+                for file in $plugin_path/conf.d/*.fish
+                    emit (string match -r $_plug_regex $file)[2]_uninstall
+                end
+
+                for file in $plugin_path/functions/*.fish
+                    functions -e (string match -r $_plug_regex $file)[2]
+                end
+
+                for file in $plugin_path/completions/*.fish
+                    complete -e (string match -r $_plug_regex $file)[2]
+                end
+
+                set -e _plug_enabled[$index]
+                echo plug: (set_color -o)$plugin(set_color normal) disabled
+            end
         case update up
         case pin
         case unpin
         case "*"
             echo plug: Unknown command (set_color -o)$cmd(set_color normal) >&2
             return 1
+    end
+
+    if set -q _plug_error
+        return 1
     end
 end
